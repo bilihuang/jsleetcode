55ca38d6d1ad2824da13ec14b9650bd3a75edc2c
menghan18 <menghan18@users.noreply.github.com>
2019-04-03 11:27:00 +0800

    sort-array-by-parity-ii

---
+++ code/sort/lesson3.js
@@ -0,0 +1,16 @@
+export default (arr) => {
+  arr.sort((a, b) => a - b)
+  let res = []
+  let odd = 1
+  let even = 0
+  arr.forEach(item => {
+    if (item % 2 === 0) {
+      res[even] = item
+      even += 2
+    } else {
+      res[odd] = item
+      odd += 2
+    }
+  })
+  return res
+}
